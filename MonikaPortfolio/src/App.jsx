2069e1699238fbba0764d63f8deb931a7b74661d
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2025-07-05 19:09:38 +0530

    app

--- MonikaPortfolio/src/App.jsx
+++ MonikaPortfolio/src/App.jsx
@@ -1,39 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-
-// Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Education from "./pages/Education";
+import Certifications from "./pages/Certifications";
 
 const App = () => {
   return (
-    <Router>
-      <ToastContainer/>
-      <div >
-        <Navbar />
+    <>
+      <ToastContainer />
+      <Navbar />
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
+      {/* All Sections in One Page */}
+      <Home />
+      <About />
+      <Education/>
+      <Skills />
+      <Projects />
+      <Certifications/>
+      <Contact />
 
-        <Footer />
-      </div>
-    </Router>
+      <Footer />
+    </>
   );
 };
 
